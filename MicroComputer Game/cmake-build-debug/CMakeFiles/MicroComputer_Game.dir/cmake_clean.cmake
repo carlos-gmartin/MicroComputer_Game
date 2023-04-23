08ad7c4830e36766c8eb69e92d913658910c2727
Carlos Gerardo Martin <cm43833@gmail.com>
2023-04-23 22:53:30 +0100

    New MicroComputer 2D Game

---
+++ MicroComputer Game/cmake-build-debug/CMakeFiles/MicroComputer_Game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MicroComputer_Game.dir/main.cpp.obj"
+  "MicroComputer_Game.exe"
+  "MicroComputer_Game.exe.manifest"
+  "MicroComputer_Game.pdb"
+  "libMicroComputer_Game.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MicroComputer_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ MicroComputer Game/cmake-build-debug/CMakeFiles/MicroComputer_Game.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MicroComputer_Game.
+# This may be replaced when dependencies are built.
